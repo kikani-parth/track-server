25f161307bc5c4640aa88fdaf005981fc24be584
Parth Kikani <parthkikani02@gmail.com>
2024-09-18 11:54:16 +0300

    implement middleware to verify token

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -24,8 +25,8 @@ mongoose.connection.on('error', () => {
   console.error('Error connecting to mongo', error);
 });
 
-app.get('/', (req, res) => {
-  res.send('there');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Email: ${req.user.email}`);
 });
 
 app.listen(PORT, () => {
