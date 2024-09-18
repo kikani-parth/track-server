6df38ac9025b05d92acf063ffad7399a47a6afd4
Parth Kikani <parthkikani02@gmail.com>
2024-09-18 11:32:22 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+  'mongodb+srv://admin:admin@tracker-cluster.pie0m.mongodb.net/?retryWrites=true&w=majority&appName=tracker-cluster';
+
+const PORT = 3000;
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', () => {
+  console.error('Error connecting to mongo', error);
+});
+
+app.get('/', (req, res) => {
+  res.send('there');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
