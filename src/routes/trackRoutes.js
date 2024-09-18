5c58679a7c0e959287d45dce8ec01034de06e343
Parth Kikani <parthkikani02@gmail.com>
2024-09-18 13:02:12 +0300

    implement route to add a track

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -10,9 +10,28 @@ router.use(requireAuth); // requires user to be logged in
 
 router.get('/tracks', async (req, res) => {
   //const userId = req.user._id;
-  const tracks = await Track.find({ userId: req.user._id });
+  const tracks = await Track.find({ userId: req.body._id });
 
   res.send(tracks);
+});
+
+router.post('/tracks', async (req, res) => {
+  const { name, locations } = req.body;
+
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .send({ error: 'You must provide name and locations' });
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.body._id });
+    await track.save();
+
+    res.send(track);
+  } catch (error) {
+    res.status(422).send({ error: error.message });
+  }
 });
 
 module.exports = router;
